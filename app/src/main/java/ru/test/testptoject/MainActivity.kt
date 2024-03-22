f28069b101037a2e96445b7583e9bdbab3d839b0
Eremin Danila <danilka.eremin@gmail.com>
2024-03-22 10:39:48 +0800

    esstu-2 ДОбавляется китайский язык

--- app/src/main/java/ru/test/testptoject/MainActivity.kt
+++ app/src/main/java/ru/test/testptoject/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.test.testptoject.ui.theme.TestPtojectTheme
 
-// test
+// китайский язык добавляется
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
