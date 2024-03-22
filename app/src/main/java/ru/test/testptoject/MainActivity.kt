2131efac039341849abcefcbe835df6fd0b8d3c5
Eremin Danila <danilka.eremin@gmail.com>
2024-03-22 10:38:10 +0800

    esstu-1 тест

--- app/src/main/java/ru/test/testptoject/MainActivity.kt
+++ app/src/main/java/ru/test/testptoject/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.test.testptoject.ui.theme.TestPtojectTheme
 
+// test
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
